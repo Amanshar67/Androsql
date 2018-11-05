541d6d5abc495bb9c4a74ee1b7eb49dfbc5b806d
Amanshar67 <amanshar67@gmail.com>
2018-11-05 16:01:04 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,10 +4,10 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password 
-define('DB_DATABASE', "androsql"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', "2748278_blog"); // db user
+define('DB_PASSWORD', "blog2@laravel"); // db password 
+define('DB_DATABASE', "2748278_blog"); // database name
+define('DB_SERVER', "fdb21.awardspace.net"); // db server
 
 $connect = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
 
